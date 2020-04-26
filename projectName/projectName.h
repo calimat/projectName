c75b1f54c27523b29dcc823f77ab8a34686de5c0
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2020-04-26 00:16:33 -0500

    Initial Commit

---
+++ projectName/projectName.h
@@ -0,0 +1,19 @@
+//
+//  projectName.h
+//  projectName
+//
+//  Created by Ricardo Herrera Petit on 4/26/20.
+//  Copyright © 2020 Ricardo Herrera Petit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for projectName.
+FOUNDATION_EXPORT double projectNameVersionNumber;
+
+//! Project version string for projectName.
+FOUNDATION_EXPORT const unsigned char projectNameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <projectName/PublicHeader.h>
+
+
